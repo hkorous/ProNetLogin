f6ae78a4444dccf8a5de1bdbef79c5915a677e24
HKorous <hkorous@prohcn.com>
2022-04-20 11:49:18 -0700

    Login Page 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
-import LoginPage2 from './Pages/LoginPage2';
 
 const App = () => {
     return (
       <>
-        <LoginPage2 />
+        <LoginPage />
       </>
     );
   }
